5063d7afef66ad9cce747e18a4695894114f195e
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-06-17 15:05:17 +0400

    Added banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
+<section id="home_banner">
+    <div class="container">
+        <div class="col-md-6">
+            <h1>Lodge Your Income Tax Return the Smart, Easy Way</h1>
+            <p>Trusted by individuals and families across Australia. Secure. Simple. Fast.</p>
+            <a href="#" class="navbar_btn">Start Your Income Tax Return Now</a>
+        </div>
+    </div>
+</section>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -26,7 +27,7 @@
             </div>
         @endif
         @include('layouts.navbar')
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
         @include('layouts.footer')
