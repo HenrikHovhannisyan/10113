ff0ce26fb7d160458198c2257a1c144038e44151
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-07-17 16:36:38 +0400

    End front Dividend Deductions  form

--- resources/views/forms/deduction/dividend_deduction.blade.php
+++ resources/views/forms/deduction/dividend_deduction.blade.php
@@ -1,6 +1,21 @@
 <form>
-    <div class="d-flex align-items-center justify-content-between mb-3">
-        <h4 class="form_title">Dividend Deductions</h4>
-        <img src="{{ asset('img/icons/help.png') }}" alt="Help">
+  <div class="d-flex align-items-center justify-content-between mb-3">
+    <h4 class="form_title">Dividend Deductions</h4>
+    <img src="{{ asset('img/icons/help.png') }}" alt="Help">
+  </div>
+<p class="choosing-business-type-text">
+    You need to complete this section if you have any deductions related to dividend income that you have earned.
+</p>
+  <div class="grin_box_border p-3 mb-3">
+    <p class="choosing-business-type-text mb-3">E.g. Account keeping or managing fees</p>
+    <div class="col-md-6 mb-3">
+      <label class="choosing-business-type-text">Dividend deduction description</label>
+      <input type="text" class="form-control border-dark" name="dividend_description" placeholder="...">
     </div>
+
+    <div class="col-md-6 mb-3">
+      <label class="choosing-business-type-text">Total amount you paid</label>
+      <input type="text" class="form-control border-dark" name="dividend_amount" placeholder="00.00$">
+    </div>
+  </div>
 </form>
