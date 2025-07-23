81d3af3b1aa12fbfe1fb483488d9a0445b3be4f2
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-07-23 17:44:37 +0400

    End front Australian Superannuation Income Stream form

--- resources/views/forms/other-details/super_income_stream.blade.php
+++ resources/views/forms/other-details/super_income_stream.blade.php
@@ -1,6 +1,17 @@
 <form>
     <div class="d-flex align-items-center justify-content-between mb-3">
-        <h4 class="form_title">Zone / Overseas Forces Offset</h4>
+        <h4 class="form_title">Australian Superannuation Income Stream</h4>
         <img src="{{ asset('img/icons/help.png') }}" alt="Help">
     </div>
+    <div class="grin_box_border p-3 mb-5">
+
+    <div class="col-md-6 mb-3">
+        <label class="choosing-business-type-text mb-2" for="superannuation_income">
+            Australian superannuation income stream
+        </label>
+        <input type="number" step="0.01" class="form-control" id="superannuation_income" name="superannuation_income" placeholder="00.00$">
+    </div>
+
+</div>
+
 </form>
