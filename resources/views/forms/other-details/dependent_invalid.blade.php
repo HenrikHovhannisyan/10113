e570e8cd77ae4dcf54ffc7d18cceae3e7023b8d5
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-07-25 15:41:00 +0400

    End front Dependent (invalid and carer) form

--- resources/views/forms/other-details/dependent_invalid.blade.php
+++ resources/views/forms/other-details/dependent_invalid.blade.php
@@ -3,4 +3,14 @@
         <h4 class="form_title">Dependent (invalid and carer)</h4>
         <img src="{{ asset('img/icons/help.png') }}" alt="Help">
     </div>
+    <div class="grin_box_border p-3 mb-4">
+
+    <div class="col-md-6 mb-3">
+        <label for="invalid_carer_offset" class="choosing-business-type-text mb-2">
+            Invalid and invalid carer tax offset
+        </label>
+        <input type="number" step="0.01" class="form-control border-dark" id="invalid_carer_offset" name="invalid_carer_offset" placeholder="00.00$">
+    </div>
+</div>
+
 </form>
