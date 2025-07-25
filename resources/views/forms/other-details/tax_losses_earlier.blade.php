68b50ebfd2fa6c6f72aad51a6e9a566321be4c14
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-07-25 15:37:16 +0400

    End front Tax Losses Of Earlier Income Years Claimed This Income Year form

--- resources/views/forms/other-details/tax_losses_earlier.blade.php
+++ resources/views/forms/other-details/tax_losses_earlier.blade.php
@@ -1,6 +1,43 @@
 <form>
     <div class="d-flex align-items-center justify-content-between mb-3">
-        <h4 class="form_title">Superannuation Contributions on Behalf of Your Spouse</h4>
+        <h4 class="form_title">Tax Losses Of Earlier Income Years Claimed This Income Year</h4>
         <img src="{{ asset('img/icons/help.png') }}" alt="Help">
     </div>
+    <div class="grin_box_border p-3 mb-4">
+        <!-- Primary production losses -->
+        <div class="row mb-3">
+            <div class="col-md-6">
+                <label class="choosing-business-type-text mb-2" for="primary_losses_carried">
+                    Primary production losses carried forward from earlier income years
+                </label>
+                <input type="number" step="0.01" class="form-control border-dark" id="primary_losses_carried" name="primary_losses_carried" placeholder="00.00$">
+            </div>
+
+            <div class="col-md-6">
+                <label class="choosing-business-type-text mb-2" for="primary_losses_claimed">
+                    Primary production losses claimed this income year
+                </label>
+                <input type="number" step="0.01" class="form-control border-dark" id="primary_losses_claimed" name="primary_losses_claimed" placeholder="00.00$">
+            </div>
+        </div>
+
+        <!-- Non-primary production losses -->
+        <div class="row">
+            <div class="col-md-6">
+                <label class="choosing-business-type-text mb-2" for="non_primary_losses_carried">
+                    Non-primary production losses carried forward from earlier income years
+                </label>
+                <input type="number" step="0.01" class="form-control border-dark" id="non_primary_losses_carried" name="non_primary_losses_carried" placeholder="00.00$">
+            </div>
+
+            <div class="col-md-6">
+                <label class="choosing-business-type-text mb-2" for="non_primary_losses_claimed">
+                    Non-primary production losses claimed this income year
+                </label>
+                <input type="number" step="0.01" class="form-control border-dark" id="non_primary_losses_claimed" name="non_primary_losses_claimed" placeholder="00.00$">
+            </div>
+        </div>
+
+    </div>
+
 </form>
--- resources/views/tabs/other-details.blade.php
+++ resources/views/tabs/other-details.blade.php
@@ -53,6 +53,7 @@
         <div class="d-none" id="other-details-form-6">@include('forms.other-details.medical_expenses')</div>
         <div class="d-none" id="other-details-form-9">@include('forms.other-details.super_income_stream')</div>
         <div class="d-none" id="other-details-form-10">@include('forms.other-details.super_contributions_spouse')</div>
+        <div class="d-none" id="other-details-form-11">@include('forms.other-details.tax_losses_earlier')</div>
         @include('forms.other-details.form.mls')
         <div class="d-none" id="other-details-form-5">@include('forms.other-details.tax_free_threshold')</div>
         <div class="d-none" id="other-details-form-7">@include('forms.other-details.under_18')</div>
@@ -61,7 +62,6 @@
         <div class="d-none" id="other-details-form-0">@include('forms.other-details.spouse_details')</div>
         @include('forms.other-details.form.attach')
         <div class="d-none" id="other-details-form-1">@include('forms.other-details.private_health_insurance')</div>
-        <div class="d-none" id="other-details-form-11">@include('forms.other-details.tax_losses_earlier')</div>
         <div class="d-none" id="other-details-form-12">@include('forms.other-details.dependent_invalid')</div>
         <div class="d-none" id="other-details-form-13">@include('forms.other-details.super_co_contribution')</div>
         <div class="d-none" id="other-details-form-14">@include('forms.other-details.other_refundable_offsets')</div>
