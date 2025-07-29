1c61e3a9965c5696279d3810e93fbc58dd03151e
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-07-29 15:39:21 +0400

    Added icons Income forms

--- resources/views/tabs/income.blade.php
+++ resources/views/tabs/income.blade.php
@@ -3,221 +3,68 @@
     <p class="choosing-business-type-text text-center">
         Enter the personal information we need to verify your identity and begin your tax refund application.
     </p>
-    <div class="select_income_container">
-        <div class="income-item" data-index="0">
-            <div class="income-label">
-                <p>Salary / Wages</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
+    <div class="select_deduction_container select_deduction_container2 mt-0">
+        @php
+            $incomeItems = [
+                'salary' => 'Salary / Wages',
+                'interest' => 'Interest',
+                'dividends' => 'Dividends',
+                'government_allowances' => 'Government Allowances',
+                'government_pension' => 'Government Pension',
+                'capital_gains' => 'Capital Gains or Losses',
+                'managed_funds' => 'Managed Funds',
+                'termination_payments' => 'Termination Payments',
+                'rent' => 'Rent Received',
+                'partnerships' => 'Partnerships and Trusts',
+                'annuities' => 'Australian Annuities',
+                'superannuation' => 'Superannuation Income Stream',
+                'super_lump_sums' => 'Super Lump Sums',
+                'ess' => 'Employee Share Schemes',
+                'personal_services' => 'Personal Services Income',
+                'business_income' => 'Income / Loss From Business',
+                'business_losses' => 'Deferred Business Losses',
+                'foreign_income' => 'Foreign Source Income',
+                'other_income' => 'Other Income'
+            ];
+        @endphp
+
+        @foreach($incomeItems as $key => $label)
+            <div class="other-details-item {{ $loop->index > 5 ? 'hidden' : '' }}" data-index="{{ $loop->index }}">
+                <div class="other-details-label">
+                    <p>{{ $label }}</p>
+                    <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
+                </div>
+                <div class="other-details-icon">
+                    <img src="{{ asset('img/icons/income/' . $key . '.png') }}" class="img-fluid" alt="{{ $label }}">
+                </div>
             </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item" data-index="1">
-            <div class="income-label">
-                <p>Interest</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item" data-index="2">
-            <div class="income-label">
-                <p>Dividends</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item" data-index="3">
-            <div class="income-label">
-                <p>Government Allowances</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item" data-index="4">
-            <div class="income-label">
-                <p>Government Pension</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item" data-index="5">
-            <div class="income-label">
-                <p>Capital Gains or Losses</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="6">
-            <div class="income-label">
-                <p>Managed Funds</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="7">
-            <div class="income-label">
-                <p>Termination Payments</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="8">
-            <div class="income-label">
-                <p>Rent Received</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="9">
-            <div class="income-label">
-                <p>Partnerships and Trusts</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="10">
-            <div class="income-label">
-                <p>Australian Annuities</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="11">
-            <div class="income-label">
-                <p>Superannuation Income Stream</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="12">
-            <div class="income-label">
-                <p>Super Lump Sums</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="13">
-            <div class="income-label">
-                <p>Employee Share Schemes</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="14">
-            <div class="income-label">
-                <p>Personal Services Income</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="15">
-            <div class="income-label">
-                <p>Income / Loss From Business</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="16">
-            <div class="income-label">
-                <p>Deferred Business Losses</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="17">
-            <div class="income-label">
-                <p>Foreign Source Income</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
-        <div class="income-item hidden" data-index="18">
-            <div class="income-label">
-                <p>Other Income</p>
-                <img src="{{ asset('img/icons/hr.png') }}" class="img-fluid" alt="hr">
-            </div>
-            <div class="income-icon">
-                <img src="{{ asset('img/image.png') }}" class="img-fluid" alt="image">
-            </div>
-        </div>
+        @endforeach
     </div>
 
     <div class="text-center">
-        <button class="btn toggle-btn" id="toggleBtn">Show More</button>
+        <button class="btn toggle-btn" id="toggleBtnIncome">Show More</button>
     </div>
 </section>
+
 <section class="choosing-business-type_section">
     <h2 class="choosing-business-type-title" id="forms_title">Let’s add the details</h2>
     <div class="form-container">
-        <div class="d-none" id="form-0">@include('forms.income.salary')</div>
-        <div class="d-none" id="form-1">@include('forms.income.interest')</div>
-        <div class="d-none" id="form-2">@include('forms.income.dividends')</div>
-        <div class="d-none" id="form-3">@include('forms.income.government_allowances')</div>
-        <div class="d-none" id="form-4">@include('forms.income.government_pension')</div>
-        <div class="d-none" id="form-5">@include('forms.income.capital_gains')</div>
-        <div class="d-none" id="form-6">@include('forms.income.managed_funds')</div>
-        <div class="d-none" id="form-7">@include('forms.income.termination_payments')</div>
-        <div class="d-none" id="form-8">@include('forms.income.rent')</div>
-        <div class="d-none" id="form-9">@include('forms.income.partnerships')</div>
-        <div class="d-none" id="form-10">@include('forms.income.annuities')</div>
-        <div class="d-none" id="form-11">@include('forms.income.superannuation')</div>
-        <div class="d-none" id="form-12">@include('forms.income.super_lump_sums')</div>
-        <div class="d-none" id="form-13">@include('forms.income.ess')</div>
-        <div class="d-none" id="form-14">@include('forms.income.personal_services')</div>
-        <div class="d-none" id="form-15">@include('forms.income.business_income')</div>
-        <div class="d-none" id="form-16">@include('forms.income.business_losses')</div>
-        <div class="d-none" id="form-17">@include('forms.income.foreign_income')</div>
-        <div class="d-none" id="form-18">@include('forms.income.other_income')</div>
+        @foreach(array_keys($incomeItems) as $i => $key)
+            <div class="d-none" id="form-{{ $i }}">@include('forms.income.' . $key)</div>
+        @endforeach
     </div>
 </section>
 
 <script>
 document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".income-item");
+    const items = document.querySelectorAll(".other-details-item");
 
     items.forEach((item) => {
         item.addEventListener("click", () => {
             const index = item.getAttribute("data-index");
             const formToShow = document.getElementById(`form-${index}`);
 
-            if (!formToShow) return;
-
-            if (formToShow.classList.contains("d-none")) {
+            if (formToShow && formToShow.classList.contains("d-none")) {
                 formToShow.classList.remove("d-none");
                 item.classList.add("active");
 
@@ -228,6 +75,11 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    const toggleBtn = document.getElementById("toggleBtnIncome");
+    toggleBtn.addEventListener("click", function () {
+        document.querySelectorAll(".other-details-item.hidden").forEach(el => el.classList.remove("hidden"));
+        toggleBtn.classList.add("d-none");
+    });
 });
 </script>
-
