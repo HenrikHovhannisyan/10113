1c1c80bcfbaa81b4bb27ed27a22fb812099f76a6
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-08-20 16:45:22 +0400

    Low Value Pool Deduction

--- resources/views/forms/deduction/low_value_pool.blade.php
+++ resources/views/forms/deduction/low_value_pool.blade.php
@@ -1,4 +1,4 @@
-<form>
+<section>
   <div class="d-flex align-items-center justify-content-between mb-3">
     <h4 class="form_title">Low Value Pool Deduction</h4>
     <img src="{{ asset('img/icons/help.png') }}" alt="Help">
@@ -10,29 +10,22 @@
         <label class="choosing-business-type-text">
           Low value pool deductions relating to financial investments
         </label>
-        <input type="text" class="form-control border-dark" name="lvp_financial" placeholder="00.00$">
+        <input type="text" class="form-control border-dark" name="low_value_pool[lvp_financial]" value="{{ old('low_value_pool.lvp_financial', $deductions->low_value_pool['lvp_financial'] ?? '') }}" placeholder="00.00$">
       </div>
 
       <div class="col-md-6 mb-3">
         <label class="choosing-business-type-text">
           Low value pool deductions relating to rental properties
         </label>
-        <input type="text" class="form-control border-dark" name="lvp_rental" placeholder="00.00$">
+        <input type="text" class="form-control border-dark" name="low_value_pool[lvp_rental]" value="{{ old('low_value_pool.lvp_rental', $deductions->low_value_pool['lvp_rental'] ?? '') }}" placeholder="00.00$">
       </div>
 
       <div class="col-md-6 mb-3">
         <label class="choosing-business-type-text">
           Other low value pool deductions
         </label>
-        <input type="text" class="form-control border-dark" name="lvp_other" placeholder="00.00$">
+        <input type="text" class="form-control border-dark" name="low_value_pool[lvp_other]"value="{{ old('low_value_pool.lvp_other', $deductions->low_value_pool['lvp_other'] ?? '') }}"  placeholder="00.00$">
       </div>
     </div>
   </div>
-
-  <div class="row">
-    <div class="col-md-6 mb-3">
-      <label class="choosing-business-type-text">Total low value pool:</label>
-      <p class="choosing-business-type-text text-secondary" id="lvp_total">00.00$</p>
-    </div>
-  </div>
-</form>
+</section>
